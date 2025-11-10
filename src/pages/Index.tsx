39544dc26427d167d3e61aecd162267d39df27bf
[dyad] <matheuscostamelo63@gmail.com>
2025-11-09 22:59:36 -0300

    Reverted all changes back to version b975914773ae92b19bbf6e8dc3ce1d727215e4dd

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,6 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import Projects from "@/components/Projects";
-import Clients from "@/components/Clients";
 import Contact from "@/components/Contact";
 
 const Index = () => {
@@ -12,7 +11,6 @@ const Index = () => {
       <About />
       <Services />
       <Projects />
-      <Clients />
       <Contact />
     </div>
   );
