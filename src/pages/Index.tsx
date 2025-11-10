241639b90b751043742ea78a112d0883acd63a67
[dyad] <matheuscostamelo63@gmail.com>
2025-11-09 22:59:07 -0300

    [dyad] Adicionando seção de Clientes com carrossel de logos - wrote 5 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import Projects from "@/components/Projects";
+import Clients from "@/components/Clients";
 import Contact from "@/components/Contact";
 
 const Index = () => {
@@ -11,6 +12,7 @@ const Index = () => {
       <About />
       <Services />
       <Projects />
+      <Clients />
       <Contact />
     </div>
   );
